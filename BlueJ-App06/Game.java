f60e8cec40f36ce47e82ec3e3948d020b146012e
Alex Clements <71945393+clements1992@users.noreply.github.com>
2020-12-09 16:28:26 +0000

    Added Hint
    Lets the user type HINT in any of the rooms and will eventually give them a hint, similar to help.

--- BlueJ-App06/Game.java
+++ BlueJ-App06/Game.java
@@ -93,7 +93,7 @@ public class Game
         System.out.println();
         System.out.println("Welcome to the World of Zuul!");
         System.out.println("World of Zuul is a new, incredibly boring adventure game.");
-        System.out.println("Type '" + CommandWord.HELP + "' if you need help.");
+        System.out.println("Type '" + CommandWord.HELP + "' if you need help. or " CommandWord.HINT " for a hint" );
         System.out.println();
         System.out.println(currentRoom.getLongDescription());
     }
