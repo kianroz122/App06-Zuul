40aed39e0a771a4e30d61d0f63585ef6738ed2f5
Alex Clements <71945393+clements1992@users.noreply.github.com>
2020-12-10 08:49:42 +0000

    all rooms moved to map class

--- BlueJ-App06/Map.java
+++ BlueJ-App06/Map.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 /**
  * Write a description of class Map here.
  *
@@ -9,27 +9,45 @@
 /** THIS IS A TEST DELETE THIS LINE */
 public class Map
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private Room outside;
+    private Room revolutions, pub, lab, office, house;
+    private Room startRoom;
 
-    /**
-     * Constructor for objects of class Map
-     */
     public Map()
     {
-        // initialise instance variables
-        x = 0;
+        createRooms();
+        startRoom = outside; //Game starts outside
+    }
+    /**
+     * Create all the rooms and link their exits together.
+     */
+    private void createRooms()
+    {
+        Room outside, revolutions, pub, lab, office, house;
+
+        // create the rooms
+        outside = new Room("outside the main entrance of the university");
+        revolutions = new Room("in rev's Beaconsfield");
+        pub = new Room("in the campus pub");
+        lab = new Room("in a computing lab");
+        office = new Room("in the computing admin office");
+        house = new Room("At your house, you've made it home");
+
+        // initialise room exits
+        outside.setExit("east", revolutions);
+        outside.setExit("south", lab);
+        outside.setExit("west", pub);
+        outside.setExit("north", house);
+
+        revolutions.setExit("west", outside);
+
+        pub.setExit("east", outside);
+
+        lab.setExit("north", outside);
+        lab.setExit("east", office);
+
+        office.setExit("west", lab);
+        
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
